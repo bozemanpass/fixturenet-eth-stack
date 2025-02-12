5561dc903dc32f4eced6e6834eeddbc1a2c92514
Thomas E Lackey <telackey@bozemanpass.com>
2025-02-11 21:16:23 -0600

    stock lcli

--- container-build/bpi-lighthouse-cli/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Build bpi/lighthouse-cli
-
-source ${BPI_CONTAINER_BASE_DIR}/build-base.sh
-
-project_dir=${BPI_REPO_BASE_DIR}/lighthouse
-docker build -t bpi/lighthouse-cli:local --build-arg PORTABLE=true -f ${project_dir}/lcli/Dockerfile ${build_command_args} ${project_dir}
